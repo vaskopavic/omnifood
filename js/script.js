7168abff5952dee7ce57c3519d1d907e8eec8e17
Vasko Pavic <vaskopavic@gmail.com>
2023-04-26 15:53:50 +0200

    redesign

--- js/script.js
+++ js/script.js
@@ -44,7 +44,6 @@ const sectionHeroEl = document.querySelector(".section-hero");
 const obs = new IntersectionObserver(
   function (entries) {
     const ent = entries[0];
-    console.log(ent);
 
     if (ent.isIntersecting === false) {
       document.body.classList.add("sticky");
@@ -77,7 +76,6 @@ function checkFlexGap() {
   document.body.appendChild(flex);
   var isSupported = flex.scrollHeight === 1;
   flex.parentNode.removeChild(flex);
-  console.log(isSupported);
 
   if (!isSupported) document.body.classList.add("no-flexbox-gap");
 }
